f268b4af933b0d3bf8d577b2310e8ff889499aec
opilane <opilane@HP-CZC0359NNL>
2024-05-16 09:45:14 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine());
+            int number =  int.Parse(Console.ReadLine());
+            if (number < 5)
+            {
+                Console.WriteLine("number on [he kuni viie vahel");
+            }
+            else
+            {
+                Console.WriteLine("number on viiest suurem");
+            }
+        }
+    }
+}
